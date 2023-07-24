38a25623cf6b08c56eb57de5e78fb761f0388a79
znznzn707 <znznzn707@hotmail.com>
2023-07-24 22:31:11 +0800

    smart config

--- main/wifi_sta.c
+++ main/wifi_sta.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
+#include <string.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <esp_system.h>
-#include <esp_spi_flash.h>
+// #include <esp_spi_flash.h>
 #include <esp_log.h>
 #include <esp_err.h>
 #include <esp_wifi.h>
 #include <esp_event.h>
 #include <event_groups.h>
+#include <esp_smartconfig.h>
 #include "wifi_sta.h"
 
 #define WIFI_STA_CONED_BIT      (1 << 0)
@@ -16,14 +17,7 @@
 
 static const char *s_pbyTag = "wifi_sta";
 
-
 static EventGroupHandle_t s_hWifiStaEventGroup;
-static wifi_config_t s_wifiConfig = {
-    .sta = {
-        .ssid = "abc",
-        .password = "12345678",  
-    },
-};
 
 static void wifi_event_handler(void *event_handler_arg,
                                esp_event_base_t event_base,
@@ -49,8 +43,53 @@ static void wifi_event_handler(void *event_handler_arg,
             ip_event_got_ip_t *pIpEventGotIp = (ip_event_got_ip_t *) event_data;
             ESP_LOGI(s_pbyTag, "got ip=%s\n", ip4addr_ntoa( &pIpEventGotIp->ip_info.ip ) );
         }
+    }else if(event_base == SC_EVENT)
+    {
+        if(event_id == SC_EVENT_GOT_SSID_PSWD)
+        {
+            wifi_config_t wifi_config = {0};
+            smartconfig_event_got_ssid_pswd_t *evt = (smartconfig_event_got_ssid_pswd_t *)event_data;
+            memcpy(wifi_config.sta.ssid, evt->ssid, sizeof(wifi_config.sta.ssid));
+            memcpy(wifi_config.sta.password, evt->password, sizeof(wifi_config.sta.password));
+            ESP_LOGI(s_pbyTag, "got ssid=%s\n", evt->ssid );
+            ESP_LOGI(s_pbyTag, "got password=%s\n", evt->password );
+            
+            ESP_ERROR_CHECK( esp_wifi_disconnect() );
+            ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+            ESP_ERROR_CHECK( esp_wifi_connect() ); //连接ap
+        }
     }
 }
+
+
+static void smart_init_config()
+{
+    smartconfig_start_config_t smartconfig_start_config = SMARTCONFIG_START_CONFIG_DEFAULT();
+
+    EventBits_t eventBits;
+
+    ESP_ERROR_CHECK( esp_smartconfig_set_type(SC_TYPE_AIRKISS) );
+    ESP_ERROR_CHECK( esp_event_handler_register(SC_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL) );
+    ESP_ERROR_CHECK( esp_smartconfig_start(&smartconfig_start_config) ) ;
+    ESP_LOGI(s_pbyTag, "smart config started...\n");
+
+    eventBits = xEventGroupWaitBits(s_hWifiStaEventGroup,
+                                    WIFI_STA_CONED_BIT | WIFI_STA_DISCONED_BIT,
+                                    pdTRUE,
+                                    pdFALSE,
+                                    portMAX_DELAY);
+
+    if (eventBits & WIFI_STA_CONED_BIT)
+    {
+        ESP_LOGI(s_pbyTag, "smart config has connected to ap...\n");
+    }
+    else if (eventBits & WIFI_STA_DISCONED_BIT)
+    {
+        ESP_LOGI(s_pbyTag, "smart config diconnected to ap...\n");
+    }
+    ESP_ERROR_CHECK(esp_event_handler_unregister( SC_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler) );
+}
+
 /**
  * @brief wifi sta初始化
  *
@@ -58,6 +97,13 @@ static void wifi_event_handler(void *event_handler_arg,
 void wifi_init_sta()
 {
     wifi_init_config_t wifiInitConfig = WIFI_INIT_CONFIG_DEFAULT();
+    wifi_config_t wifiConfig = {
+        .sta = {
+            .ssid = "abc",
+            .password = "12345678",
+        },
+    };
+
     s_hWifiStaEventGroup = xEventGroupCreate();
     EventBits_t eventBits;
 
@@ -70,27 +116,28 @@ void wifi_init_sta()
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL));
 
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &s_wifiConfig));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifiConfig));
     ESP_ERROR_CHECK(esp_wifi_start()); // 启动wifi，但还没有连接
+    ESP_LOGI(s_pbyTag, "wifi init sta started...\n");
 
     eventBits = xEventGroupWaitBits(s_hWifiStaEventGroup,
                                     WIFI_STA_CONED_BIT | WIFI_STA_DISCONED_BIT,
-                                    pdFALSE,
+                                    pdTRUE,
                                     pdFALSE,
                                     portMAX_DELAY);
 
     if (eventBits & WIFI_STA_CONED_BIT)
     {
-        ESP_LOGI(s_pbyTag, "wifi sta has connected to ap...\n");
+        ESP_LOGI(s_pbyTag, "wifi init sta has connected to ap...\n");
     }
     else if (eventBits & WIFI_STA_DISCONED_BIT)
     {
-        ESP_LOGI(s_pbyTag, "wifi sta diconnected to ap...\n");
+        ESP_LOGI(s_pbyTag, "wifi init sta diconnected to ap...\n");
+        smart_init_config();
     }
 
     // 卸载事件
     ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler));
     ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler));
     vEventGroupDelete(s_hWifiStaEventGroup);
-    // ESP_ERROR_CHECK();
 }
